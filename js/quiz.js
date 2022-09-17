62b019db6c82c5ceca9c9ca6f8d181089a54cb5c
Amine <amine.belfkira99@gmail.com>
2022-09-18 00:11:13 +0200

    new

--- js/quiz.js
+++ js/quiz.js
@@ -76,116 +76,128 @@ var quiz = {
     "JS": [
         {
             "id": 1,
-            "question": "Brass gets discoloured in air because of the presence of which of the following gases in air?",
+            "question": "It is easier to roll a stone up a sloping road than to lift it vertical upwards because?",
             "options": [
                 {
-                    "a": "&lt;script&gt;",
-                    "b": "&lt;javascript&gt;",
-                    "c": "&lt;scripting&gt;",
-                    "d": "&lt;js&gt;"
+                    "a": "work done in rolling is more than in lifting",
+                    "b": "work done in lifting the stone is equal to rolling it",
+                    "c": "work done in both is same but the rate of doing work is less in rolling",
+                    "d": "work done in rolling a stone is less than in lifting it"
+
 
                 }
             ],
-            "answer": "&lt;script&gt;",
+            "answer": "work done in rolling a stone is less than in lifting it",
             "score": 0,
             "status": ""
         },
         {
             "id": 2,
-            "question": "Where is the correct place to insert a JavaScript?",
+            "question": "Which of the following measurements is not a unit of distance?",
             "options": [
                 {
-                    "a": "The &lt;head&gt; section",
-                    "b": "The &lt;body&gt; section",
-                    "c": "Both the &lt;head&gt; section and the &lt;body&gt; section are correct"
+                    "a": "Ammeter",
+                    "b": "Cubit",
+                    "c": "Parsec",
+                    "d": "angstrom"
                 }
             ],
-            "answer": "Both the &lt;head&gt; section and the &lt;body&gt; section are correct",
+            "answer": "Ammeter",
             "score": 0,
             "status": ""
+
+
+
         },
         {
             "id": 3,
-            "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
+            "question": "Energy travels from Sun to Earch through",
             "options": [
                 {
-                    "a": "&ltscript href=&quot;xxx.js&quot;>",
-                    "b": "&lt;script name=&quot;xxx.js&quot;&gt;",
-                    "c": "&lt;script src=&quot;xxx.js&quot;&gt;"
+                    "a": "conduction",
+                    "b": "convection",
+                    "c": "radiation",
+                    "d": "modulation"
                 }
             ],
-            "answer": "&lt;script src=&quot;xxx.js&quot;&gt;",
+            "answer": "radiation",
             "score": 0,
             "status": ""
         },
         {
             "id": 4,
-            "question": "The external JavaScript file must contain the &lt;script&gt; tag.",
+            "question": "Scalars are quantities that are described by _____________",
             "options": [
                 {
-                    "a": "True",
-                    "b": "False"
+                    "a": "Magnitude",
+                    "b": "Direction",
+                    "c": "Magnitude & Direction",
+                    "d": "Motion"
                 }
             ],
-            "answer": "False",
+            "answer": "Magnitude",
             "score": 0,
             "status": ""
         },
         {
             "id": 5,
-            "question": "How do you write &quot;Hello World&quot; in an alert box?",
+            "question": "Albert Einstein was awarded Nobel Prize for his path-breaking research and formulation of the:",
             "options": [
                 {
-                    "a": "alertBox(&quot;Hello World&quot;);",
-                    "b": "msg(&quot;Hello World&quot;);",
-                    "c": "alert(&quot;Hello World&quot;);",
-                    "d": "msgBox(&quot;Hello World&quot;);",
+                    "a": "Theory of Relavitity",
+                    "b": "Laws of Photo-Electric Effect",
+                    "c": "Principle of Wave-Particle Duality",
+                    "d": "Theory of Critical Opalescence"
                 }
             ],
-            "answer": "alert(&quot;Hello World&quot;);",
+            "answer": "Laws of Photo-Electric Effect",
             "score": 0,
             "status": ""
         },
         {
             "id": 6,
-            "question": "How do you create a function in JavaScript?",
+            "question": "Which of the following are components of Central Processing Unit (CPU)?",
             "options": [
                 {
-                    "a": "function myFunction()",
-                    "b": "function:myFunction()",
-                    "c": "function = myFunction()",
+                    "a": "Arithmetic logic unit, Mouse",
+                    "b": "Arithmetic logic unit, Control unit",
+                    "c": "Arithmetic logic unit, Integrated Circuits",
+                    "d": "Control Unit, Monitor"
+
                 }
             ],
-            "answer": "function myFunction()",
+            "answer": "Arithmetic logic unit, Control unit",
             "score": 0,
             "status": ""
         },
         {
             "id": 7,
-            "question": "How do you call a function named &quot;myFunction&quot;?",
+            "question": "One kilobyte (KB) is equal to",
             "options": [
                 {
-                    "a": "call function myFunction()",
-                    "b": "call myFunction()",
-                    "c": "myFunction()",
+                    "a": "1,000 bits",
+                    "b": "1,024 bytes",
+                    "c": "1,024 megabytes",
+                    "d": "1,024 gigabytes"
                 }
             ],
-            "answer": "myFunction()",
+            "answer": "1,000 bits",
             "score": 0,
             "status": ""
         },
         {
             "id": 8,
-            "question": "How to write an IF statement in JavaScript?",
+            "question": "The rules of a language is known as ____",
             "options": [
                 {
-                    "a": "if i = 5 then",
-                    "b": "if i == 5 then",
-                    "c": "if (i == 5)",
-                    "d": " if i = 5",
+                    "a": "Code",
+                    "b": "Structure",
+                    "c": "Procedure",
+                    "d": "Syntax",
+
                 }
             ],
-            "answer": "if (i == 5)",
+            "answer": "Syntax",
             "score": 0,
             "status": ""
         },
@@ -206,232 +218,22 @@ var quiz = {
         },
         {
             "id": 10,
-            "question": "How to write an IF statement for executing some code if &quot;i&quot; is NOT equal to 5?",
+            "question": "The common name for the crime of stealing passwords is _______",
             "options": [
                 {
-                    "a": "if (i <> 5)",
-                    "b": "if i <> 5",
-                    "c": "if (i != 5)",
-                    "d": "if i =! 5 then",
+                    "a": "spooling",
+                    "b": "identity theft",
+                    "c": "spoofing",
+                    "d": "hacking ",
+
                 }
+
             ],
-            "answer": "if (i != 5)",
+            "answer": "spoofing",
             "score": 0,
             "status": ""
         },
-        {
-            "id": 11,
-            "question": "How does a WHILE loop start?",
-            "options": [
-                {
-                    "a": "while i = 1 to 10",
-                    "b": "while (i &lt;= 10; i++)",
-                    "c": "while (i &lt;= 10)"
-                }
-            ],
-            "answer": "while (i &lt;= 10)",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 12,
-            "question": "How does a FOR loop start?",
-            "options": [
-                {
-                    "a": "for (i = 0; i &lt;= 5)",
-                    "b": "for (i = 0; i &lt;= 5; i++)",
-                    "c": "for i = 1 to 5",
-                    "d": "for (i &lt;= 5; i++)"
-                }
-            ],
-            "answer": "for (i = 0; i &lt;= 5; i++)",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 13,
-            "question": "How can you add a comment in a JavaScript?",
-            "options": [
-                {
-                    "a": "//This is a comment",
-                    "b": "&sbquo;This is a comment",
-                    "c": "&lt;!--This is a comment--&gt;"
-                }
-            ],
-            "answer": "//This is a comment",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 14,
-            "question": "How to insert a comment that has more than one line?",
-            "options": [
-                {
-                    "a": "/*This comment has more than one line*/",
-                    "b": "//This comment has more than one line//",
-                    "c": "&lt;!--This comment has more than one line--&gt;"
-                }
-            ],
-            "answer": "/*This comment has more than one line*/",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 15,
-            "question": "What is the correct way to write a JavaScript array?",
-            "options": [
-                {
-                    "a": "var colors = (1:&quot;red&quot;, 2:&quot;green&quot;, 3:&quot;blue&quot;)",
-                    "b": "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
-                    "c": "var colors = 1 = (&quot;red&quot;), 2 = (&quot;green&quot;), 3 = (&quot;blue&quot;)",
-                    "d": "var colors = &quot;red&quot;, &quot;green&quot;, &quot;blue&quot;"
-                }
-            ],
-            "answer": "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 16,
-            "question": "How do you round the number 7.25, to the nearest integer?",
-            "options": [
-                {
-                    "a": "rnd(7.25)",
-                    "b": "Math.round(7.25)",
-                    "c": "Math.rnd(7.25)",
-                    "d": "round(7.25)"
-                }
-            ],
-            "answer": "Math.round(7.25)",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 17,
-            "question": "How do you find the number with the highest value of x and y?",
-            "options": [
-                {
-                    "a": "Math.max(x, y)",
-                    "b": "Math.ceil(x, y)",
-                    "c": "top(x, y)",
-                    "d": "ceil(x, y)"
-                }
-            ],
-            "answer": "Math.max(x, y)",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 18,
-            "question": "What is the correct JavaScript syntax for opening a new window called &quot;w2&quot;?",
-            "options": [
-                {
-                    "a": "w2 = window.new(&quot;http://www.w3schools.com&quot;);",
-                    "b": "w2 = window.open(&quot;http://www.w3schools.com&quot;);"
-                }
-            ],
-            "answer": "w2 = window.open(&quot;http://www.w3schools.com&quot;);",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 19,
-            "question": "JavaScript is the same as Java.",
-            "options": [
-                {
-                    "a": "true",
-                    "b": "false"
-                }
-            ],
-            "answer": "false",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 20,
-            "question": "How can you detect the client&rsquo;s browser name?",
-            "options": [
-                {
-                    "a": "navigator.appName",
-                    "b": "browser.name",
-                    "c": "client.navName"
-                }
-            ],
-            "answer": "navigator.appName",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 21,
-            "question": "Which event occurs when the user clicks on an HTML element?",
-            "options": [
-                {
-                    "a": "onchange",
-                    "b": "onclick",
-                    "c": "onmouseclick",
-                    "d": "onmouseover"
-                }
-            ],
-            "answer": "onclick",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 22,
-            "question": "How do you declare a JavaScript variable?",
-            "options": [
-                {
-                    "a": "var carName;",
-                    "b": "variable carName;",
-                    "c": "v carName;"
-                }
-            ],
-            "answer": "var carName;",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 23,
-            "question": "Which operator is used to assign a value to a variable?",
-            "options": [
-                {
-                    "a": "*",
-                    "b": "-",
-                    "c": "=",
-                    "d": "x"
-                }
-            ],
-            "answer": "=",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 24,
-            "question": "What will the following code return: Boolean(10 &gt; 9)",
-            "options": [
-                {
-                    "a": "NaN",
-                    "b": "false",
-                    "c": "true"
-                }
-            ],
-            "answer": "true",
-            "score": 0,
-            "status": ""
-        },
-        {
-            "id": 25,
-            "question": "Is JavaScript case-sensitive?",
-            "options": [
-                {
-                    "a": "No",
-                    "b": "Yes"
-                }
-            ],
-            "answer": "Yes",
-            "score": 0,
-            "status": ""
-        }
+
     ]
 }
 
